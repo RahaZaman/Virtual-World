39c5f8a32c858b656590f5fb0efc1544f4a88918
RahaZaman <rahamatzaman@gmail.com>
2025-05-01 01:01:36 -0700

    first commit

---
+++ Cube.js
@@ -0,0 +1,78 @@
+class Cube {
+
+    constructor() {
+        this.type = "cube";
+        this.color = [1.0, 1.0, 1.0, 1.0];
+        this.matrix = new Matrix4();
+    }
+
+    render() {
+
+        var rgba = this.color;
+
+        gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+        gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+
+        // Front of cube
+        drawTriangle3DUV([0.0,0.0, 1.1,0, 1.0,0 ], [1.0, 0.1, 1.1]);
+        drawTriangle3D([0.0,0.0, 0.1,0, 1.1,0 ]);
+
+        // Pass the color of a point to u_FragColor uniform variable
+        gl.uniform4f(u_FragColor, rgba[0]*.9, rgba[1]*.9, rgba[2]*.9, rgba[3]);
+
+        // Top of cube
+        drawTriangle3D([0.1,0, 0.1,1, 1.1,1]);
+        drawTriangle3D([0.1,0, 1.1,1, 1.1,0]);
+
+        // drawCube();
+    }
+}
+
+function drawCube() {
+    let vertices = new Float32Array([
+      // Front face
+      -0.5, -0.5,  0.5,
+       0.5, -0.5,  0.5,
+       0.5,  0.5,  0.5,
+      -0.5,  0.5,  0.5,
+      // Back face
+      -0.5, -0.5, -0.5,
+       0.5, -0.5, -0.5,
+       0.5,  0.5, -0.5,
+      -0.5,  0.5, -0.5,
+    ]);
+  
+    let indices = new Uint8Array([
+      0,1,2,   0,2,3,    // front
+      1,5,6,   1,6,2,    // right
+      5,4,7,   5,7,6,    // back
+      4,0,3,   4,3,7,    // left
+      3,2,6,   3,6,7,    // top
+      4,5,1,   4,1,0     // bottom
+    ]);
+  
+    // Create a buffer object
+    var vertexBuffer = gl.createBuffer();
+    var indexBuffer = gl.createBuffer();
+    if (!vertexBuffer || !indexBuffer) {
+      console.log('Failed to create buffers');
+      return -1;
+    }
+  
+    // Write vertex coordinates to buffer object
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+  
+    // Assign buffer object to a_Position variable
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+  
+    // Enable assignment
+    gl.enableVertexAttribArray(a_Position);
+  
+    // Write indices to the buffer object
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+  
+    // Draw cube
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_BYTE, 0);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Virtual World 
+*CSE 160: Introduction to Computer Graphics*
